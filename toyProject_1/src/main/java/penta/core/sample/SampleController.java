2d2cbc574c4cb7305802daf195dfb985672121d5
User <User@DESKTOP-FRB54J1>
2021-07-04 14:16:59 +0900

    test

---
+++ toyProject_1/src/main/java/penta/core/sample/SampleController.java
@@ -0,0 +1,14 @@
+package penta.core.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+	@RequestMapping("/test")
+	public @ResponseBody String test() {
+		return "test";
+	}
+}
